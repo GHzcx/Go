5e9d84700041e54c83b784498074c798417f3535
zhengguanghui <zhengguanghui@58.com>
2020-05-13 20:11:21 +0800

    fix

--- src/main/main.go
+++ src/main/main.go
@@ -9,4 +9,9 @@ func main() {
 	fmt.Println("hello world")
 	fmt.Println(spider.GetProxyUrl())
 	fmt.Println(spider.List)
+	url := "https://app.api.ke.com/Rentplat/v1/house/total?city_id=110000&condition=ht1ab200301001000&request_ts=1589371036&scene=list"
+	//url := "https://m.ke.com/chuzu/bj/brand/pg1/?ajax=1"
+	spider.Download(url)
+	h := fmt.Sprintf("%s, %s", "hah", "HH")
+	fmt.Print(h)
 }
--- src/spider/spider.go
+++ src/spider/spider.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"math/rand"
 	"net/http"
+	"net/url"
 	"strings"
 	"time"
 )
@@ -19,7 +20,7 @@ type HouseInfo struct {
 	ShopNum int
 }
 
-type AllData map[string][]HouseInfo
+type AllData map[string][]HouseInfo //映射关系 城市 =》公寓信息
 
 //获取代理IP
 func GetProxyUrl() string {
@@ -42,6 +43,51 @@ func GetProxyUrl() string {
 	str := strings.ToLower(f[num]["proxy_type"]) +"://" + f[num]["ip"] + ":" + f[num]["port"]
 	return str
 }
+
+//填充每个房源下的字段信息
+func (h HouseInfo)UpdateOnlineInfo(abbr string) {
+
+}
+
+//获取url内容函数
+func Download(urlLink string) string {
+	proxy := func(_ *http.Request) (*url.URL, error) {
+		return url.Parse(GetProxyUrl())
+	}
+	client := &http.Client{
+		Transport: &http.Transport{Proxy: proxy},
+	}
+	req,_ := http.NewRequest(http.MethodGet, urlLink, nil)
+	req.Header.Set("Cookie", "lianjia_uuid=D54C855E-409C-4938-BE3C-9C0923F8AF58; lianjia_ssid=6EC4B1B6-BBA8-4396-8DA6-7C966DF2C877; lianjia_udid=A253C17E-5837-4AF8-8523-06F3AF7C5851; lianjia_token=2.003a4d930e46e84eaf2be0ba3fb4ad3b3f")
+	req.Header.Add("Lianjia-Access-Token", "2.003a4d930e46e84eaf2be0ba3fb4ad3b3f")
+	req.Header.Add("Lianjia-Device-Id", "A253C17E-5837-4AF8-8523-06F3AF7C5851")
+	req.Header.Add("Authorization", "MjAxODAxMTFfaW9zOjk4MDVjNGUwZDI1MWZhNWE3ZjU2NTE0NTNlNjM0OWM3MTg2MzllODY=")
+	resp, err := client.Do(req)
+	fmt.Println(err)
+	for err != nil {
+		resp, err = client.Do(req)
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("read body error")
+	}
+	fmt.Println(string(body))
+	return ""
+}
+
+//获取城市公寓信息
+func GetBrandInfoList(cityCode, abbr string) []HouseInfo {
+	for page :=0; ; page++ {
+		detailUrl := fmt.Sprintf("https://m.ke.com/chuzu/%s/brand/pg%d/?ajax=1", abbr, page)
+		Download(detailUrl)
+	}
+}
+
+
+
+
 
 
 
